bcd4a4c4ba05cca84ad0f739d28e751128c1a550
Pantea Stefan <32839016+panteastefan@users.noreply.github.com>
2021-07-10 12:25:11 +0300

    crated GitHubSearch component

---
+++ github_gist_api/src/components/GitHubSearch.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class GitHubSearch extends Component {
+    render(){
+        return <h1>GitHubSearch component</h1>
+    }
+}
+
+export default GitHubSearch;
