e98ed7dbafcaea3aeb368653e79c924af2829bf1
Pantea Stefan <panteastefan97@yahoo.com>
2021-08-05 11:49:30 +0300

    removed isOdd reducer

--- github_gist_api/src/reducers/index.js
+++ github_gist_api/src/reducers/index.js
@@ -1,10 +1,8 @@
 import counterReducer from "./counter";
-import oddReducer from "./isOdd";
 import { combineReducers } from "redux";
 
 const allReducers = combineReducers({
   counter: counterReducer,
-  isOdd: oddReducer,
 });
 
 export default allReducers;
